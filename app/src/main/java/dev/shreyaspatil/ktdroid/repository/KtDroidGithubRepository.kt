ff9ab98459ec04566f618decf51fd97214e16b0a
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-29 22:56:51 +0530

    Created KtDroidGitHubRepository to get data

---
+++ app/src/main/java/dev/shreyaspatil/ktdroid/repository/KtDroidGithubRepository.kt
@@ -0,0 +1,23 @@
+package dev.shreyaspatil.ktdroid.repository
+
+import dev.shreyaspatil.ktdroid.api.GitHubService
+import dev.shreyaspatil.ktdroid.model.Repositories
+import dev.shreyaspatil.ktdroid.utils.State
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.Response
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+class KtDroidGitHubRepository(private val githubService: GitHubService) {
+
+    fun getRepositories(): Flow<State<Repositories>> {
+        return object : NetworkBoundRepository<Repositories>() {
+            override suspend fun fetchFromRemote(): Response<Repositories> =
+                githubService.getRepositories()
+        }.asFlow().flowOn(Dispatchers.IO)
+    }
+}
