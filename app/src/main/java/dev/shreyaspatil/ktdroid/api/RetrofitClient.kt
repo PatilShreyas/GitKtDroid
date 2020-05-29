cdb50d1ca42028c6bb4567232fd573a7e583eda9
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-29 22:57:40 +0530

    Created Retrofit client builder

---
+++ app/src/main/java/dev/shreyaspatil/ktdroid/api/RetrofitClient.kt
@@ -0,0 +1,45 @@
+package dev.shreyaspatil.ktdroid.api
+
+import android.content.Context
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dev.shreyaspatil.ktdroid.utils.isNetworkAvailable
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@ExperimentalCoroutinesApi
+fun getGitHubService(context: Context) =
+    Retrofit.Builder()
+        .baseUrl(GitHubService.BASE_URL)
+        .addConverterFactory(
+            MoshiConverterFactory.create(
+                Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+            )
+        )
+        .client(getOkHttpClient(context))
+        .build()
+        .create(GitHubService::class.java)
+
+
+fun getOkHttpClient(context: Context): OkHttpClient {
+    val cacheSize = (5 * 1024 * 1024).toLong()
+    val myCache = Cache(context.cacheDir, cacheSize)
+
+    return OkHttpClient.Builder()
+        .cache(myCache)
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = if (isNetworkAvailable(context)!!)
+                request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+            else
+                request.newBuilder().header(
+                    "Cache-Control",
+                    "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7
+                ).build()
+            chain.proceed(request)
+        }
+        .build()
+}
