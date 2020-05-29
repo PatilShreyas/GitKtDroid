cc9187123de7505b4ce96e7cb35f3ec9e6ab2a97
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-29 22:55:44 +0530

    Created Repository model

---
+++ app/src/main/java/dev/shreyaspatil/ktdroid/model/Repository.kt
@@ -0,0 +1,31 @@
+package dev.shreyaspatil.ktdroid.model
+
+import com.squareup.moshi.Json
+
+data class Repositories(
+    val items: List<Repository>
+)
+
+data class Repository(
+    @Json(name = "full_name")
+    val fullName: String,
+
+    @Json(name = "stargazers_count")
+    val stars: Int,
+
+    @Json(name = "html_url")
+    val repoUrl: String,
+
+    val description: String,
+    val owner: Owner
+)
+
+data class Owner(
+    @Json(name = "login")
+    val name: String,
+
+    @Json(name = "avatar_url")
+    val avatarUrl: String
+
+
+)
