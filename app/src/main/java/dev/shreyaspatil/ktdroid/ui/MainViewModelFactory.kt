58a5f449a5d2da0feb119125aae8c451f24cfc21
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-29 23:00:21 +0530

    Created factory for MainViewModel

---
+++ app/src/main/java/dev/shreyaspatil/ktdroid/ui/MainViewModelFactory.kt
@@ -0,0 +1,22 @@
+package dev.shreyaspatil.ktdroid.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dev.shreyaspatil.ktdroid.api.getGitHubService
+import dev.shreyaspatil.ktdroid.repository.KtDroidGitHubRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.InternalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@FlowPreview
+@InternalCoroutinesApi
+class MainViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(KtDroidGitHubRepository::class.java).newInstance(
+            getGitHubService(context)
+        )
+    }
+}
