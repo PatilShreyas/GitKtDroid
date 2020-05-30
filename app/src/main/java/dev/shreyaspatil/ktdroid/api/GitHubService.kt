788af6ee51eb07bdcb0a936bf050799d2c1f97df
Shreyas Patil <shreyaspatilg@gmail.com>
2020-05-30 22:21:51 +0530

    Added methods to get user and repository of user

--- app/src/main/java/dev/shreyaspatil/ktdroid/api/GitHubService.kt
+++ app/src/main/java/dev/shreyaspatil/ktdroid/api/GitHubService.kt
@@ -1,13 +1,22 @@
 package dev.shreyaspatil.ktdroid.api
 
 import dev.shreyaspatil.ktdroid.model.Repositories
+import dev.shreyaspatil.ktdroid.model.Repository
+import dev.shreyaspatil.ktdroid.model.User
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface GitHubService {
 
-    @GET("/search/repositories?q=android+language:kotlin&sort=stars&order=desc")
-    suspend fun getRepositories(): Response<Repositories>
+    @GET("/search/repositories?q=android+language:kotlin&sort=stars&order=desc&per_page=100")
+    suspend fun getKtAndroidRepositories(): Response<Repositories>
+
+    @GET("/users/{user}/")
+    suspend fun getUser(@Path("user") username: String): Response<User>
+
+    @GET("/users/{user}/repos?per_page=10000000")
+    suspend fun getRepositoriesByUser(@Path("user") username: String): Response<List<Repository>>
 
     companion object {
         const val BASE_URL = "https://api.github.com/"
